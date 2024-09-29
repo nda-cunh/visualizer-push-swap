8ea95381a09796b38371436bbb6cad323dc6f76a
Nda-cunh <hydraldev@gmail.com>
2024-09-29 04:08:40 +0200

    Remove Makefile to install.sh

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+echo ''
+echo '███████╗██╗  ██╗██████╗ ██████╗  █████╗'
+echo '██╔════╝██║  ██║██╔══██╗██╔══██╗██╔══██╗'
+echo '███████╗██║  ██║██████╔╝██████╔╝███████║'
+echo '╚════██║██║  ██║██╔═══╝ ██╔══██╗██╔══██║'
+echo '███████║╚█████╔╝██║     ██║  ██║██║  ██║'
+echo '╚══════╝ ╚════╝ ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝'
+echo ''
+
+# install meson or ninja if not exist
+
+if ! [ -x "$(command -v meson)" ] || ! [ -x "$(command -v ninja)" ]; then 
+	echo "meson not found, installing..."
+	pip install meson ninja --user
+fi
+
+export PATH=$PATH:$HOME/.local/bin
+
+if ! [ -x "$(command -v meson)" ] || ! [ -x "$(command -v ninja)" ]; then 
+	echo "meson install failed, please install manually"
+	echo ""
+	echo "	Ubuntu    (apt install meson ninja-build)"
+	echo "	Fedora    (dnf install meson ninja-build)"
+	echo "	ArchLinux (pacman -S meson ninja)"
+	exit 1
+fi
+	
+# build if build/build.ninja doesnt exist
+
+if [ ! -f build/build.ninja ]; then
+	echo "building..."
+	meson build --prefix="$PWD" --bindir='' --optimization=3 --buildtype=release
+fi
+ninja install -C build
